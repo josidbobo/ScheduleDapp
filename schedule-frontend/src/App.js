355bbee1af39410d5a35b85e93927c282c126c0e
Joshua <josidbobo>
2022-06-17 23:58:24 +0100

    updated

--- schedule-frontend/src/App.js
+++ schedule-frontend/src/App.js
@@ -6,29 +6,29 @@ import './style.css';
 
 function App() {
   const [tasks, setTasks] = useState(undefined);
-  const [todo, setTodo] = useState(undefined);
+  const [schedule, setSchedule] = useState(undefined);
 
   useEffect(() => {
      const init = async () => {
-       const { todo } = await getBlockchain();
-       const tasks = await todo.getTasks();
-       setTodo(todo);
+       const { schedule } = await getBlockchain();
+       const tasks = await schedule.getTasks();
+       setSchedule(schedule);
        setTasks(tasks);
      };
      init();
   }, []);
 
   const createTask = async (content, author) => {
-    const tx = await todo.createTask(content, author)
+    const tx = await schedule.createTask(content, author)
     await tx.wait();
-    const tasks = await todo.getTasks(); 
+    const tasks = await schedule.getTasks(); 
     setTasks(tasks);
   }
 
   const toggleDone = async id => {
-    const tx = await todo.toggleDone(id);
+    const tx = await schedule.toggleDone(id);
     await tx.wait();
-    const tasks = await todo.getTasks(); 
+    const tasks = await schedule.getTasks(); 
     setTasks(tasks);
   }
 
